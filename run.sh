f0756a14f06a2143011ed25a233f6d08a693b383
Henry <henryhbrl@gmail.com>
2018-07-24 18:07:01 -0300

    changing command line and name of input file

--- run.sh
+++ run.sh
@@ -1,1 +1,1 @@
-./spr_supertree -lgt_analysis -lgt_visualization -i 0 -initial_tree ./tests/SPR-MRP-Rooting.tree < ./tests/MRP_rooted.trees
+./spr_supertree -lgt_visualization -i 0 -initial_tree ./tests/SPR-MRP-Rooting.tree < ./tests/MRP_rooted_only399.trees
